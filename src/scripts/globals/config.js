47fc2da9676d710919eab0d3f14b766dd7795a91
Samuel Yusa <Samuelyusa@gmail.com>
2023-05-12 23:04:09 +0700

    First Commit

---
+++ src/scripts/globals/config.js
@@ -0,0 +1,8 @@
+const CONFIG = {
+    KEY: 'facc9398a1973d590db59729cf20c800',
+    BASE_URL: 'https://api.themoviedb.org/3/',
+    BASE_IMAGE_URL: 'https://image.tmdb.org/t/p/w500/',
+    DEFAULT_LANGUAGE: 'en-us',
+};
+
+export default CONFIG;
