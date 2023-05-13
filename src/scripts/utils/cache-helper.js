b275fd5c8e55a5b732ec2a14b4129062b1c49bea
Samuel Yusa <Samuelyusa@gmail.com>
2023-05-13 21:54:16 +0700

    update cache-helper

--- src/scripts/utils/cache-helper.js
+++ src/scripts/utils/cache-helper.js
@@ -1,29 +1,28 @@
-import CONFIG from "../globals/config";
+import CONFIG from '../globals/config';
 
 const CacheHelper = {
-    async cachingAppShell(requests){
+    async cachingAppShell(requests) {
         const cache = await this._openCache();
         cache.addAll(requests);
     },
-    
-    async deleteOldCache(){
+
+    async deleteOldCache() {
         const cacheNames = await caches.keys();
         cacheNames
-            .filter((name) => name !== CONFIG.CACHE_NAME)
-            .map((filteredName) => caches.delete(filteredName));
+        .filter((name) => name !== CONFIG.CACHE_NAME)
+        .map((filteredName) => caches.delete(filteredName));
     },
 
     async revalidateCache(request) {
         const response = await caches.match(request);
 
         if (response) {
-            this._fetchRequest(request);
-            return response;
+        this._fetchRequest(request);
+        return response;
         }
-
         return this._fetchRequest(request);
     },
-    
+
     async _openCache() {
         return caches.open(CONFIG.CACHE_NAME);
     },
@@ -32,7 +31,7 @@ const CacheHelper = {
         const response = await fetch(request);
 
         if (!response || response.status !== 200) {
-            return response;
+        return response;
         }
 
         await this._addCache(request);
